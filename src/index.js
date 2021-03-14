246b1636f81f76e573fd89633303b6d6875e5661
Dmitriy Babich <babich-d@mail.ru>
2021-03-14 20:07:12 +0600

    feat: add js brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let existBracket = {};
+  for(var j = 0; j < bracketsConfig.length; j += 1) {
+    existBracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+  }
+
+  stack.push(str.charAt(0));
+
+  for(let i = 1; i < str.length; i += 1) {
+    if(str.charAt(i) === existBracket[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(str.charAt(i)); 
+    }
+  }
+  if (stack.length > 0) {
+    return false;
+  }
+  return true;
+};
